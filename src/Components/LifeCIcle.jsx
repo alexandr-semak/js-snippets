ef1dab2be198928a9c15d297880213c0af0de0f1
Kamesas <semakaleksandr2014@gmail.com>
2018-08-27 16:05:18 +0300

    LifeCycle

--- src/Components/LifeCIcle.jsx
+++ src/Components/LifeCIcle.jsx
@@ -3,14 +3,37 @@ import React, { Component } from "react";
 class LifeCicle extends Component {
   constructor(props) {
     super(props);
-    console.log("LifeCicle constructor");
+    console.log("1 - constructor(state) LifeCicle");
     this.state = {
       arg: props.arg
     };
   }
 
+  //WARNING! To be deprecated in React v17. Use componentDidMount instead.
+  componentWillMount() {
+    console.log("2 - componentWillMount(deprecated)");
+  }
+
+  componentDidMount() {
+    console.log("4 - componentDidMount (jquery, счетчик, сетевый запросы, работа с DOM)");
+  }
+
+  componentWillUnmount() {
+    console.log(" - componentWillUnmount (плагины jQuery, clearInterval, удаляем Listeners)");
+  }
+
+  //WARNING! To be deprecated in React v17. Use componentDidUpdate instead.
+  componentWillUpdate(nextProps, nextState) {
+    console.log(" 5 - componentWillUpdate ");
+  }
+
   render() {
-    return <div>Жизненный цикл</div>;
+    console.log("3 - render");
+    return (
+      <div>
+        <h1>Жизненный цикл</h1>
+      </div>
+    );
   }
 }
 
---
+++ src/Components/UpdateComponent.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class UpdateComponent extends Component {
+  state = {  }
+  render() { 
+    return (  );
+  }
+}
+ 
+export default UpdateComponent;
