0f80427dc5cf6d324e22aff2468cf7bf1f47380b
Kamesas <semakaleksandr2014@gmail.com>
2018-08-27 13:22:09 +0300

    test2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Test from "./Components/test";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Test />
         start
       </div>
     );
